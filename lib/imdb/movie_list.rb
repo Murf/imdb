8055c0ce95f74518275c024e4ea0c9e49e781133
Brett Murphy <brett@cleverweb.com.au>
2013-01-10 19:28:28 +1100

    better handling of aka, example title L'immortel (2010), aka '22 Bullets'

--- lib/imdb/movie_list.rb
+++ lib/imdb/movie_list.rb
@@ -12,13 +12,14 @@ module Imdb
         element.parent.innerHTML =~ /media from/i
       end.map do |element|
         id = element['href'][/\d+/]
-        
-        data = element.parent.innerHTML.split("<br />")
-        if !data[0].nil? && !data[1].nil? && data[0] =~ /img/
-          title = data[1]
-        else
-          title = data[0]
-        end
+
+        title = element.parent.innerHTML
+        #data = element.parent.innerHTML.split("<br />")
+        #if !data[0].nil? && !data[1].nil? && data[0] =~ /img/
+        #  title = data[1]
+        #else
+        #  title = data[0]
+        #end
         
         title = title.imdb_strip_tags.imdb_unescape_html
         title.gsub!(/\s+\(\d\d\d\d\)$/, '')
@@ -30,7 +31,11 @@ module Imdb
           title = titles.shift.strip.imdb_unescape_html
           alternative_titles = titles.map { |t| t.strip.imdb_strip_tags.imdb_unescape_html }
         end
-        
+
+        alternative_titles.each do |aka|
+          title += ", aka '"+aka+"'"
+        end
+
         [id, title, alternative_titles]
       end.uniq.map do |values|
         Imdb::Movie.new(*values)
